d7f1b5a5cdd0569217d75d09904dd1576153e89f
Johnson_Shen <chaogedebishi@sjtu.edu.cn>
2017-11-21 00:56:11 -0500

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/AESCPP.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AESCPP.dir/main.cpp.obj"
+  "AESCPP.pdb"
+  "AESCPP.exe"
+  "AESCPP.exe.manifest"
+  "libAESCPP.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AESCPP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
